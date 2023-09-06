d68ac5f3af78c8169cdb6e59ff10b0ba75eef2b6
Namithaingnit <namitha.nair@ignitarium.com>
2023-09-06 10:17:19 +0530

    fourth commit

--- sum.c
+++ sum.c
@@ -3,8 +3,8 @@
 int main() {
    int a, b ,c ,d ,sum;
  
-   printf("\nEnter four  no: ");
-   scanf("%d %d %d %d ", &a, &b ,&c ,&d);
+   printf("Enter four  no: ");
+   scanf("%d %d %d %d", &a, &b ,&c ,&d);
  
    sum  = a + b + c + d;
    printf("sum of numbers is  : %d", sum);
