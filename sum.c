3f011a418d914d077e5304e81b18352b28cacb08
Namithaingnit <namitha.nair@ignitarium.com>
2023-09-13 09:46:38 +0530

    third commit

--- sum.c
+++ sum.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
  
-int main() {
-   int a, b ,c ,sum;
- 
-   printf("Enter three   no: ");
-   scanf("%d %d %d ", &a, &b ,&c);
- 
-   sum  = a + b + c;
-   printf("sum of numbers is  : %d \n", sum);
- 
-   return(0);
+int main(){
+
+         int a, b ,c ,sum;
+         printf("Enter three no: ");
+         scanf("%d %d %d",&a,&b,&c);
+	 sum  = a + b + c;
+	 printf("sum of numbers is:%d\n",sum);
+	 return(0);
 }
+
